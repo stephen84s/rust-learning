ca8c0d97d05aeae89089dc252ec1091b7812332c
Stephen Dsouza <stephen84s@protonmail.com>
2023-08-02 00:45:03 +0400

    Move Complex to its own folder

--- ch2/ch2-for/src/main.rs
+++ ch2/ch2-for/src/main.rs
@@ -1,9 +1,52 @@
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
 fn main() {
     let sentence = vec!["Hello", "Cruel", "World", "!!!"];
+    let mut hasher = DefaultHasher::new();
     for word in &sentence {
         print!("{} ", word);
     }
 
-    // Following line will kill the program
-    println!("{}", sentence[0]);
+    println!("\n{:?}", sentence);
+
+    let mapped_vals: Vec<_> = sentence
+        .into_iter()
+        .map(|word| {
+            word.hash(&mut hasher);
+            return hasher.finish();
+        })
+        .collect();
+
+    println!("\n{:?}", mapped_vals);
+
+    // Only print odd numbers
+    for n in 0..10 {
+        if n % 2 == 0 {
+            continue;
+        }
+        println!("{}", n);
+    }
+
+    let numbers = [ 3, 59, 97, 729, 17031158983 ];
+
+    for number in numbers {
+        println!("is_prime({}): {}", number, is_prime(number));
+    }
+
+}
+
+
+fn is_prime(n: i64) -> bool {
+    if n==1 || n == 2 {
+        return true;
+    }
+
+    for i in 2..n/2 {
+        if n % i == 0 {
+            return false;
+        }
+    }
+
+    return true;
 }
